a62072542d41a4ed8e816f5f8e7e9ef5fd48c10b
Abhishek Kumar <abhi553@gmail.com>
2024-02-18 23:45:23 +0000

    get method added

--- src/test/java/com/api/framework/Getresponse.java
+++ src/test/java/com/api/framework/Getresponse.java
@@ -2,19 +2,22 @@ package com.api.framework;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * @author Abhishek Kumar on 17/02/2024
+ * @author Abhishek Kumar on 18/02/2024
  */
 public class Getresponse {
-    @Test
-    void getfirstUser(){
-        //https://reqres.in/api/users/2
-        Response res = RestAssured.get("https://reqres.in/api/users/2");
-        System.out.println("Get single user response: "+res.asString());
-        System.out.println("Get status code for single user:"+res.getStatusCode());
-
-
+@Test
+    public void test001(){
+    Response res = RestAssured.get("https://reqres.in/api/users?page=2");
+    System.out.println(res.getStatusCode());
+    System.out.println(res.getBody().asString());
+    System.out.println(res.getHeader("Content-Type"));
+    System.out.println(res.getTime());
+    int expectedStatusCode = 200;
+    int actualStatusCode = res.getStatusCode() ;
+    Assert.assertEquals(expectedStatusCode ,actualStatusCode);
     }
 }
