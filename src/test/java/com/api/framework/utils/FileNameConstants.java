a8d00692c38cbd6acd4f0c2dd0830a0880c01562
Abhishek Kumar <abhi553@gmail.com>
2024-03-12 22:16:42 +0000

    Put request

--- src/test/java/com/api/framework/utils/FileNameConstants.java
+++ src/test/java/com/api/framework/utils/FileNameConstants.java
@@ -6,4 +6,6 @@ package com.api.framework.utils;
 public class FileNameConstants {
     public static final String BASE_PATH = "src/test/resources/";
     public static final String POST_API_REQUEST_BODY = BASE_PATH+"postapirequestbody.txt";
+    public static final String TOKEN_API_REQUEST_BODY = BASE_PATH+"tokenapirequestbody.txt";
+    public static final String PUT_API_REQUEST_BODY = BASE_PATH+"putapirequestbody.txt";
 }
